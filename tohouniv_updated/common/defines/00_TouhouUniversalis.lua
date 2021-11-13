b0c4340a54c1f5b9b982c5f31323c08dee1d6ac2
AlipheeseFateburnVIII <sugoi-waifu@i.ua>
2021-11-13 19:44:11 +0100

    MKI changes
    New missions, old deleted, localisation.

--- tohouniv_updated/common/defines/00_TouhouUniversalis.lua
+++ tohouniv_updated/common/defines/00_TouhouUniversalis.lua
@@ -26,3 +26,6 @@ NDefines.NFrontend.MP_OPTIONS_POS_Y = 922.0
 NDefines.NFrontend.MP_OPTIONS_POS_Z = 700.0
 NDefines.NFrontend.MP_OPTIONS_LOOK_X = 5358.0
 NDefines.NFrontend.MP_OPTIONS_LOOK_Z = 800.0
+
+--NDefines.NCountry.ESTATE_PRIVILEGES_MAX_CONCURRENT = 8		-- Max. number of privileges active at the same time
+--NDefines.NEconomy.MAX_BUILDING_SLOTS = 16
