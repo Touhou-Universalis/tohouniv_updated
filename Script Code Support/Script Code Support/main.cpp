37ed925edbee35905aaa5214977d87f11a5e84ec
Comrade Flan <64352277+ComradeFlan@users.noreply.github.com>
2024-06-09 12:24:09 +0200

    Added code support for easy province file creation

--- Script Code Support/Script Code Support/main.cpp
+++ Script Code Support/Script Code Support/main.cpp
@@ -1,37 +1,56 @@
 #include <iostream>
 #include <string>
 #include "danmaku_duel_manager.h"
+#include "province_manager.h"
 
 int main()
 {
-	std::string effectOutputFolder = "C:\\Users\\<user name>\\Documents\\Paradox Interactive\\Europa Universalis IV\\mod\\touhou_universalis\\test";
-	std::string triggerOutputFolder = "C:\\Users\\<user name>\\Documents\\Paradox Interactive\\Europa Universalis IV\\mod\\touhou_universalis\\test";
-	std::string customLocalizationOutputFolder = "C:\\Users\\<user name>\\Documents\\Paradox Interactive\\Europa Universalis IV\\mod\\touhou_universalis\\test";
-	std::string localizationOutputFolder = "C:\\Users\\<user name>\\Documents\\Paradox Interactive\\Europa Universalis IV\\mod\\touhou_universalis\\test";
-	std::vector<std::string> combatVerbs = { "uses", "performs", "casts", "enchants", "curses", "strikes", "shoots", "attacks" };
-	std::vector<std::string> combatArticles = { "undefined", "defined" };
-	std::vector<std::string> combatAbilities = { "attack" };
-	std::vector<std::string> combatPrepositions = { "against", "on" };
-	std::vector<std::string> combatAbilityEffect = { "dealing", "healing", "restore", "afflicting_condition", "gaining_condition" };
-	std::vector<std::string> combatAbilityEndeffect = { "damage", "health", "mana", "action_points" };
+	const std::string modPath = "C:\\Users\\<user name>\\Documents\\Paradox Interactive\\Europa Universalis IV\\mod\\touhou_universalis";
+	if (false)
+	{
+		std::string effectOutputFolder = modPath + "\\output\\danmaku";
+		std::string triggerOutputFolder = modPath + "\\output\\danmaku";
+		std::string customLocalizationOutputFolder = modPath + "\\output";
+		std::string localizationOutputFolder = modPath + "\\output\\danmaku";
+		std::vector<std::string> combatVerbs = { "uses", "performs", "casts", "enchants", "curses", "strikes", "shoots", "attacks" };
+		std::vector<std::string> combatArticles = { "undefined", "defined" };
+		std::vector<std::string> combatAbilities = { "attack" };
+		std::vector<std::string> combatPrepositions = { "against", "on" };
+		std::vector<std::string> combatAbilityEffect = { "dealing", "healing", "restore", "afflicting_condition", "gaining_condition" };
+		std::vector<std::string> combatAbilityEndeffect = { "damage", "health", "mana", "action_points" };
 
-	CDanmakuDuelManager danmakuDuelManager;
-	danmakuDuelManager.SetupInstances(30);
-	danmakuDuelManager.SetupActions(15);
+		CDanmakuDuelManager danmakuDuelManager;
+		danmakuDuelManager.SetupInstances(30);
+		danmakuDuelManager.SetupActions(15);
 
-	danmakuDuelManager.SetupCombatVerbs(combatVerbs);
-	danmakuDuelManager.SetupCombatArticles(combatArticles);
-	danmakuDuelManager.SetupCombatAbilities(combatAbilities);
-	danmakuDuelManager.SetupCombatPrepositions(combatPrepositions);
-	danmakuDuelManager.SetupCombatAbilityEffects(combatAbilityEffect);
-	danmakuDuelManager.SetupCombatAbilityEndEffect(combatAbilityEndeffect);
+		danmakuDuelManager.SetupCombatVerbs(combatVerbs);
+		danmakuDuelManager.SetupCombatArticles(combatArticles);
+		danmakuDuelManager.SetupCombatAbilities(combatAbilities);
+		danmakuDuelManager.SetupCombatPrepositions(combatPrepositions);
+		danmakuDuelManager.SetupCombatAbilityEffects(combatAbilityEffect);
+		danmakuDuelManager.SetupCombatAbilityEndEffect(combatAbilityEndeffect);
 
-	danmakuDuelManager.SetupEffectOutputFolder(effectOutputFolder);
-	danmakuDuelManager.SetupTriggerOutputFolder(triggerOutputFolder);
-	danmakuDuelManager.SetupCustomizableLocalizationOutputFolder(customLocalizationOutputFolder);
-	danmakuDuelManager.SetupLocalizationOutputFolder(localizationOutputFolder);
+		danmakuDuelManager.SetupEffectOutputFolder(effectOutputFolder);
+		danmakuDuelManager.SetupTriggerOutputFolder(triggerOutputFolder);
+		danmakuDuelManager.SetupCustomizableLocalizationOutputFolder(customLocalizationOutputFolder);
+		danmakuDuelManager.SetupLocalizationOutputFolder(localizationOutputFolder);
 
-	danmakuDuelManager.GenerateCustomLocalizationFile();
-	danmakuDuelManager.GenerateEffectFile();
-	danmakuDuelManager.GenerateLocalizationFile();
+		danmakuDuelManager.GenerateCustomLocalizationFile();
+		danmakuDuelManager.GenerateEffectFile();
+		danmakuDuelManager.GenerateLocalizationFile();
+	}
+	if (false)
+	{
+		CProvinceManager provinceManager;
+		provinceManager.SetStartOfNewProvinces(7054);
+		provinceManager.SetOwner("LCU");
+		provinceManager.SetReligion("th_lunarian_religion");
+		provinceManager.SetCulture("th_lunarian_culture");
+		provinceManager.SetTradeGoods("fish");
+		
+		provinceManager.SetInputFile(modPath + "\\map\\definition.csv");
+		provinceManager.SetOutputFolder(modPath + "\\history\\provinces");
+
+		provinceManager.PrintProvinces();
+	}
 }
