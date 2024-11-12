ad9478b7d327758e25a556f50bdc4c78dee4ccb6
MrCharon <70565107+MrCharon286@users.noreply.github.com>
2024-11-13 00:42:47 +0900

    Change many Countries name

--- common/defines/00_TouhouUniversalis.lua
+++ common/defines/00_TouhouUniversalis.lua
@@ -1,31 +1,31 @@
-NDefines.NFrontend.CAMERA_LOOKAT_X = 5358.0
-NDefines.NFrontend.CAMERA_LOOKAT_Z = 1214.0
-NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_X = 5358.0
-NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_Z = 1214.0
+NDefines.NFrontend.CAMERA_LOOKAT_X = 5772.0
+NDefines.NFrontend.CAMERA_LOOKAT_Z = 1174.0
+NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_X = 5772.0
+NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_Z = 1174.0
 
-NDefines.NFrontend.CAMERA_START_X = 5358.0
-NDefines.NFrontend.CAMERA_START_Y = 700.0
-NDefines.NFrontend.CAMERA_START_Z = 1164.0
+NDefines.NFrontend.CAMERA_START_X = 5772.0
+NDefines.NFrontend.CAMERA_START_Y = 900.0
+NDefines.NFrontend.CAMERA_START_Z = 1124.0
 
-NDefines.NFrontend.CAMERA_END_X = 5358.0
-NDefines.NFrontend.CAMERA_END_Y = 800.0
-NDefines.NFrontend.CAMERA_END_Z = 1164.0
+NDefines.NFrontend.CAMERA_END_X = 5772.0
+NDefines.NFrontend.CAMERA_END_Y = 1000.0
+NDefines.NFrontend.CAMERA_END_Z = 1124.0
 
-NDefines.NFrontend.FRONTEND_POS_X = 5358.0
-NDefines.NFrontend.FRONTEND_POS_Y = 800.0
-NDefines.NFrontend.FRONTEND_POS_Z = 1164.0
-NDefines.NFrontend.FRONTEND_LOOK_X = 5358.0
-NDefines.NFrontend.FRONTEND_LOOK_Z = 1180.0
-NDefines.NFrontend.SETTINGS_POS_X = 5358.0
-NDefines.NFrontend.SETTINGS_POS_Y = 551.0
-NDefines.NFrontend.SETTINGS_POS_Z = 978.0
-NDefines.NFrontend.SETTINGS_LOOK_X = 5398.0
-NDefines.NFrontend.SETTINGS_LOOK_Z = 1160.0
-NDefines.NFrontend.MP_OPTIONS_POS_X = 5358.0
-NDefines.NFrontend.MP_OPTIONS_POS_Y = 922.0
-NDefines.NFrontend.MP_OPTIONS_POS_Z = 700.0
-NDefines.NFrontend.MP_OPTIONS_LOOK_X = 5358.0
-NDefines.NFrontend.MP_OPTIONS_LOOK_Z = 800.0
+NDefines.NFrontend.FRONTEND_POS_X = 5772.0
+NDefines.NFrontend.FRONTEND_POS_Y = 1000.0
+NDefines.NFrontend.FRONTEND_POS_Z = 1124.0
+NDefines.NFrontend.FRONTEND_LOOK_X = 5772.0
+NDefines.NFrontend.FRONTEND_LOOK_Z = 1140.0
+NDefines.NFrontend.SETTINGS_POS_X = 5772.0
+NDefines.NFrontend.SETTINGS_POS_Y = 751.0
+NDefines.NFrontend.SETTINGS_POS_Z = 918.0
+NDefines.NFrontend.SETTINGS_LOOK_X = 5812.0
+NDefines.NFrontend.SETTINGS_LOOK_Z = 1120.0
+NDefines.NFrontend.MP_OPTIONS_POS_X = 5772.0
+NDefines.NFrontend.MP_OPTIONS_POS_Y = 1122.0
+NDefines.NFrontend.MP_OPTIONS_POS_Z = 1154.0
+NDefines.NFrontend.MP_OPTIONS_LOOK_X = 5772.0
+NDefines.NFrontend.MP_OPTIONS_LOOK_Z = 1254.0
 
 NDefines.NAI.CALL_ACCEPTANCE_COALITION_VS_SUBJECT = 0
 
